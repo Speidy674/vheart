4b72785bac740b0a44668b8d50107049413756de
Justin K. <justin.k@justplayer.de>
2025-12-29 18:35:17 +0100

    .

---
+++ resources/js/pages/welcome.tsx
@@ -0,0 +1,17 @@
+import { dashboard, login, register } from '@/routes';
+import { type SharedData } from '@/types';
+import { Head, Link, usePage } from '@inertiajs/react';
+
+export default function Welcome({
+    canRegister = true,
+}: {
+    canRegister?: boolean;
+}) {
+    const { auth } = usePage<SharedData>().props;
+
+    return (
+        <>
+            <p>Hi</p>
+        </>
+    );
+}
