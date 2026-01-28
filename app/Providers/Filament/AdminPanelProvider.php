efa26687a0a90e467c54f413c76cc5fbe420528d
Justin K. <justin.k@justplayer.de>
2026-01-28 14:25:59 +0100

    disable recovery code regeneration, should require an admin later i guess

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,6 +30,7 @@ class AdminPanelProvider extends PanelProvider
             ->emailChangeVerification()
             ->multiFactorAuthentication([
                 AppAuthentication::make()
+                    ->regenerableRecoveryCodes(false)
                     ->recoverable()
                     ->brandName('VHeart'),
             ])
