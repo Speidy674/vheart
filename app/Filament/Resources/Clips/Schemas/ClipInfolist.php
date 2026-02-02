5991db2b8ed3b052b163d28f54a549f28d4ca017
Justin K. <justin.k@justplayer.de>
2026-02-03 00:28:44 +0100

    improve clip infolist layout

--- app/Filament/Resources/Clips/Schemas/ClipInfolist.php
+++ app/Filament/Resources/Clips/Schemas/ClipInfolist.php
@@ -4,12 +4,20 @@ declare(strict_types=1);
 
 namespace App\Filament\Resources\Clips\Schemas;
 
+use App\Enums\ClipVoteType;
 use App\Filament\Infolists\Components\TwitchEmbedEntry;
 use App\Filament\Resources\Users\UserResource;
 use App\Models\Clip;
+use App\Models\Game;
+use Filament\Infolists\Components\ImageEntry;
 use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Components\Grid;
+use Filament\Schemas\Components\Group;
 use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
+use Filament\Support\Enums\FontFamily;
+use Filament\Support\Enums\TextSize;
+use Filament\Support\Icons\Heroicon;
 
 class ClipInfolist
 {
@@ -18,72 +26,130 @@ class ClipInfolist
         return $schema
             ->components([
                 Section::make()
+                    ->compact()
                     ->schema([
                         TwitchEmbedEntry::make('twitch_id')
                             ->hiddenLabel()
                             ->alignCenter(),
-                    ])->columnSpan(2),
+                    ])
+                    ->columnSpan(['default' => 3, 'lg' => 2]),
+
                 Section::make()
                     ->compact()
                     ->schema([
                         TextEntry::make('title')
-                            ->label('admin/resources/clips.infolist.title')
-                            ->translateLabel(),
-                        TextEntry::make('game.title')
-                            ->label('admin/resources/clips.infolist.category')
-                            ->translateLabel(),
-                        TextEntry::make('duration')
-                            ->label('admin/resources/clips.infolist.duration')
-                            ->translateLabel()
-                            ->formatStateUsing(function ($state) {
-                                $totalSeconds = (int) round($state);
+                            ->hiddenLabel()
+                            ->weight('bold')
+                            ->size(TextSize::Large),
 
-                                $minutes = intdiv($totalSeconds, 60);
-                                $seconds = $totalSeconds % 60;
+                        Grid::make(4)
+                            ->schema([
+                                ImageEntry::make('game.box_art')
+                                    ->hiddenLabel()
+                                    ->state(fn (Game $category) => $category->getBoxArt())
+                                    ->extraImgAttributes([
+                                        'class' => 'object-cover rounded aspect-[3/4]',
+                                    ])
+                                    ->columnSpan(1)
+                                    ->grow(false),
+                                TextEntry::make('title')
+                                    ->label('admin/resources/clips.infolist.category')
+                                    ->translateLabel()
+                                    ->columnSpan(3)
+                                    ->hiddenLabel()
+                                    ->size(TextSize::Medium)
+                                    ->weight('medium'),
+                            ])
+                            ->relationship('game'),
 
-                                return sprintf('%d:%02d', $minutes, $seconds);
-                            }),
-                        TextEntry::make('broadcaster.name')
-                            ->label('admin/resources/clips.infolist.broadcaster')
-                            ->translateLabel()
-                            ->url(function (Clip $clip) {
-                                if (! $clip->broadcaster?->exists) {
-                                    return null;
-                                }
+                        Grid::make(3)
+                            ->schema([
+                                TextEntry::make('duration')
+                                    ->label(__('admin/resources/clips.table.columns.duration'))
+                                    ->tooltip(__('admin/resources/clips.table.columns.duration'))
+                                    ->icon(Heroicon::Clock)
+                                    ->formatStateUsing(fn (int $state) => gmdate('i:s', $state))
+                                    ->fontFamily(FontFamily::Mono)
+                                    ->size(TextSize::Medium)
+                                    ->badge()
+                                    ->color('gray'),
 
-                                return UserResource::getUrl('view', ['record' => $clip->broadcaster]);
-                            })->openUrlInNewTab(),
-                        TextEntry::make('creator.name')
-                            ->label('admin/resources/clips.infolist.clipper')
-                            ->translateLabel()
-                            ->url(function (Clip $clip) {
-                                if (! $clip->creator?->exists) {
-                                    return null;
-                                }
+                                TextEntry::make('votes_jury')
+                                    ->label(__('admin/resources/clips.table.columns.votes_jury'))
+                                    ->state(fn (Clip $record) => $record->votes()->where('type', ClipVoteType::Jury)->whereVoted(true)->count())
+                                    ->icon(Heroicon::Star)
+                                    ->size(TextSize::Medium)
+                                    ->badge()
+                                    ->color('warning'),
 
-                                return UserResource::getUrl('view', ['record' => $clip->creator]);
-                            })->openUrlInNewTab(),
-                        TextEntry::make('submitter.name')
-                            ->label('admin/resources/clips.infolist.submitted_by')
-                            ->translateLabel()
-                            ->url(function (Clip $clip) {
-                                if (! $clip->submitter?->exists) {
-                                    return null;
-                                }
+                                TextEntry::make('votes_public')
+                                    ->label(__('admin/resources/clips.table.columns.votes_public'))
+                                    ->state(fn (Clip $record) => $record->votes()->where('type', ClipVoteType::Public)->whereVoted(true)->count())
+                                    ->icon(Heroicon::UserGroup)
+                                    ->size(TextSize::Medium)
+                                    ->badge()
+                                    ->color('success'),
+                            ]),
 
-                                return UserResource::getUrl('view', ['record' => $clip->submitter]);
-                            })->openUrlInNewTab(),
-                        TextEntry::make('created_at')
-                            ->date()
-                            ->label('admin/resources/clips.infolist.submitted_at')
-                            ->translateLabel()
-                            ->disabled(),
-                        TextEntry::make('date')
-                            ->date()
-                            ->label('admin/resources/clips.infolist.created_at')
-                            ->translateLabel()
-                            ->disabled(),
-                    ]),
-            ])->columns(3);
+                        Group::make([
+                            TextEntry::make('broadcaster.name')
+                                ->label('admin/resources/clips.infolist.broadcaster')
+                                ->translateLabel()
+                                ->icon(Heroicon::VideoCamera)
+                                ->color('gray')
+                                ->url(function (Clip $clip) {
+                                    if (! $clip->broadcaster?->exists) {
+                                        return null;
+                                    }
+
+                                    return UserResource::getUrl('view', ['record' => $clip->broadcaster]);
+                                })->openUrlInNewTab(),
+
+                            TextEntry::make('creator.name')
+                                ->label('admin/resources/clips.infolist.clipper')
+                                ->translateLabel()
+                                ->icon(Heroicon::Scissors)
+                                ->color('gray')
+                                ->url(function (Clip $clip) {
+                                    if (! $clip->creator?->exists) {
+                                        return null;
+                                    }
+
+                                    return UserResource::getUrl('view', ['record' => $clip->creator]);
+                                })->openUrlInNewTab(),
+
+                            TextEntry::make('submitter.name')
+                                ->label('admin/resources/clips.infolist.submitted_by')
+                                ->translateLabel()
+                                ->icon(Heroicon::User)
+                                ->color('gray')
+                                ->url(function (Clip $clip) {
+                                    if (! $clip->submitter?->exists) {
+                                        return null;
+                                    }
+
+                                    return UserResource::getUrl('view', ['record' => $clip->submitter]);
+                                })->openUrlInNewTab(),
+                        ]),
+
+                        Grid::make(2)
+                            ->schema([
+                                TextEntry::make('date')
+                                    ->date()
+                                    ->label('admin/resources/clips.infolist.created_at')
+                                    ->translateLabel()
+                                    ->icon(Heroicon::Calendar)
+                                    ->color('gray'),
+                                TextEntry::make('created_at')
+                                    ->date()
+                                    ->label('admin/resources/clips.infolist.submitted_at')
+                                    ->translateLabel()
+                                    ->icon(Heroicon::Calendar)
+                                    ->color('gray'),
+                            ]),
+                    ])
+                    ->columnSpan(['default' => 3, 'lg' => 1]),
+            ])
+            ->columns(3);
     }
 }
