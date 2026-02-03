893edd1f7876fe34f6a03eace93e6893b5969673
Justin K. <justin.k@justplayer.de>
2026-02-03 02:57:45 +0100

    add user resource
    type definition was done in a different pr #132

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * Public User Data
+ *
+ * @mixin User
+ */
+class UserResource extends JsonResource
+{
+    /**
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'avatar' => $this->avatar_url,
+        ];
+    }
+}
