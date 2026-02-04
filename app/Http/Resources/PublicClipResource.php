9becfd4dcefc8557bb3e28a78acaf1368f483f07
Justin K. <justin.k@justplayer.de>
2026-02-04 20:51:40 +0100

    add mixin phpdoc for IDEs to know what model we make a resource for

--- app/Http/Resources/PublicClipResource.php
+++ app/Http/Resources/PublicClipResource.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace App\Http\Resources;
 
+use App\Models\Clip;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @mixin Clip
+ */
 class PublicClipResource extends JsonResource
 {
     /**
