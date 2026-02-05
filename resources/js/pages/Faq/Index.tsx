c906a56d99c8f65f71b34763dd26dc9faecaa95a
Speidy674 <marvin33@live.de>
2026-02-05 22:02:45 +0100

    fix faq site not loading

--- resources/js/pages/Faq/Index.tsx
+++ resources/js/pages/Faq/Index.tsx
@@ -1,16 +1,13 @@
-import { Head, usePage } from '@inertiajs/react';
 import AppHeaderLayout from '@/layouts/app/app-header-layout';
 import { FaqEntryResource, SharedData } from '@/types';
+import { Head, usePage } from '@inertiajs/react';
 
 // Note: the top bar in the layout does not work for guests yet, this has been fixed in pr 132
 // To prevent errors, login first before accessing this site for now
 export default function Index() {
     const { props } = usePage<SharedData>();
 
-    // @ts-expect-error Types will be fixed later, data wrapper will be removed in pr 132
-    const faq = props.faq.data as FaqEntryResource[];
-
-    console.log(faq);
+    const faq = props.faq as FaqEntryResource[];
     return (
         <AppHeaderLayout>
             <Head title={'FAQ'} />
