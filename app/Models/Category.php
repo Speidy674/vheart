16752f4ee063953a73ab316f4b122b8af44918af
Justin K. <justin.k@justplayer.de>
2026-02-05 05:26:13 +0100

    category permissions & policy

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\Policies\CategoryPolicy;
 use Database\Factories\CategoryFactory;
+use Illuminate\Database\Eloquent\Attributes\UsePolicy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+#[UsePolicy(CategoryPolicy::class)]
 class Category extends Model
 {
     /** @use HasFactory<CategoryFactory> */
