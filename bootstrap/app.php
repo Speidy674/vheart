befe32355a9f17689328d476bd9686c1e9f63a3e
Justin K. <justin.k@justplayer.de>
2026-02-06 07:20:12 +0100

    replace CSRF middleware with EnforceSecFetchSite middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,6 +7,7 @@ use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 use Illuminate\Session\Middleware\StartSession;
 
@@ -23,14 +24,18 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state', 'vheart_cookie_consent']);
 
-        $middleware->web(append: [
-            HandleAppearance::class,
-            HandleInertiaRequests::class,
-            AddLinkHeadersForPreloadedAssets::class,
-        ], prepend: [
-            App\Http\Middleware\StagingGateMiddleware::class,
-            App\Http\Middleware\Localization::class,
-        ]);
+        $middleware->web(
+            append: [
+                App\Http\Middleware\EnforceSecFetchSite::class,
+                HandleAppearance::class,
+                HandleInertiaRequests::class,
+                AddLinkHeadersForPreloadedAssets::class,
+            ], prepend: [
+                App\Http\Middleware\StagingGateMiddleware::class,
+                App\Http\Middleware\Localization::class,
+            ], remove: [
+                ValidateCsrfToken::class,
+            ]);
 
         $middleware->appendToPriorityList(
             StartSession::class,
