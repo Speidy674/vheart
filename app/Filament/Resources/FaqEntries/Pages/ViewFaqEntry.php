512b452055077078f2a64e17ad03cc4ed15f83fc
Justin K. <justin.k@justplayer.de>
2026-02-07 19:30:38 +0100

    create FAQ dashboard

---
+++ app/Filament/Resources/FaqEntries/Pages/ViewFaqEntry.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Resources\FaqEntries\Pages;
+
+use App\Filament\Resources\FaqEntries\FaqEntryResource;
+use Filament\Actions\EditAction;
+use Filament\Resources\Pages\ViewRecord;
+use LaraZeus\SpatieTranslatable\Actions\LocaleSwitcher;
+use LaraZeus\SpatieTranslatable\Resources\Pages\ViewRecord\Concerns\Translatable;
+
+class ViewFaqEntry extends ViewRecord
+{
+    use Translatable;
+
+    protected static string $resource = FaqEntryResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            LocaleSwitcher::make(),
+            EditAction::make(),
+        ];
+    }
+}
---
+++ app/Filament/Resources/FaqEntries/Schemas/FaqEntryInfolist.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Filament\Resources\FaqEntries\Schemas;
+
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Utilities\Get;
+use Filament\Schemas\Schema;
+
+class FaqEntryInfolist
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('title')
+                    ->reactive()
+                    ->label('admin/resources/faq-entry.form.question.label')
+                    ->translateLabel()
+                    ->placeholder(__('admin/resources/faq-entry.form.question.placeholder')),
+                DateTimePicker::make('published_at')
+                    ->label('admin/resources/faq-entry.form.published_at.label')
+                    ->translateLabel(),
+
+                Textarea::make('body')
+                    ->label('admin/resources/faq-entry.form.body.label')
+                    ->translateLabel()
+                    ->autosize()
+                    ->label('admin/resources/faq-entry.form.body.label')
+                    ->translateLabel()
+                    ->placeholder(__('admin/resources/faq-entry.form.body.placeholder'))
+                    ->columnSpanFull(),
+            ])->columns(1);
+    }
+}
