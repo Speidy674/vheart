f5447b799f6d976bd9fdc06e8fbb61902c9d4889
Justin K. <justin.k@justplayer.de>
2026-02-08 01:21:44 +0100

    add filter scopes to clip model

--- app/Models/Clip.php
+++ app/Models/Clip.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\Enums\Clips\CompilationStatus;
+use App\Enums\ClipVoteType;
 use App\Http\Resources\PublicClipResource;
 use App\Models\Clip\Compilation;
 use App\Models\Clip\CompilationClip;
@@ -12,9 +14,11 @@ use App\Models\Scopes\ClipPermissionScope;
 use App\Models\Traits\Reportable;
 use App\Policies\ClipPolicy;
 use Database\Factories\ClipFactory;
+use Illuminate\Database\Eloquent\Attributes\Scope;
 use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 use Illuminate\Database\Eloquent\Attributes\UsePolicy;
 use Illuminate\Database\Eloquent\Attributes\UseResource;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -79,5 +83,108 @@ class Clip extends Model
     public function getReportableTitleAttribute(): string
     {
         return 'title';
+    }
+
+    /**
+     * Exclude Clips that are attached to a published or scheduled Compilation
+     */
+    #[Scope]
+    protected function whereNotPublished(Builder $query): Builder
+    {
+        return $query->where(function (Builder $query) {
+            $query->whereDoesntHave('compilations', function (Builder $q) {
+                $q->whereIn('compilations.status', array_merge(
+                    CompilationStatus::getPublicCases(),
+                    [CompilationStatus::Scheduled]
+                ));
+            });
+        });
+    }
+
+    /**
+     * Exclude Clips that user has voted on
+     */
+    #[Scope]
+    protected function whereNoVotesFrom(Builder $query, User|int $userOrId): Builder
+    {
+        $userId = $userOrId instanceof User ? $userOrId->id : $userOrId;
+
+        return $query->whereDoesntHave('votes', fn (Builder $q) => $q->where('user_id', $userId));
+    }
+
+    /**
+     * Include only Clips that user has voted on
+     */
+    #[Scope]
+    protected function whereVotesFrom(Builder $query, User|int $userOrId): Builder
+    {
+        $userId = $userOrId instanceof User ? $userOrId->id : $userOrId;
+
+        return $query->whereHas('votes', fn (Builder $q) => $q->where('user_id', $userId));
+    }
+
+    /**
+     * Exclude Clips the user has Broadcasted
+     */
+    #[Scope]
+    protected function whereNotBroadcastBy(Builder $query, User|int $userOrId): Builder
+    {
+        $userId = $userOrId instanceof User ? $userOrId->id : $userOrId;
+
+        return $query->whereNot('broadcaster_id', $userId);
+    }
+
+    /**
+     * Include only Clips the user has Broadcasted
+     */
+    #[Scope]
+    protected function whereBroadcastBy(Builder $query, User|int $userOrId): Builder
+    {
+        $userId = $userOrId instanceof User ? $userOrId->id : $userOrId;
+
+        return $query->where('broadcaster_id', $userId);
+    }
+
+    /**
+     * Counts public votes as `public_votes`.
+     */
+    #[Scope]
+    protected function withPublicVoteCount(Builder $query): Builder
+    {
+        return $query->withCount(
+            [
+                'votes as public_votes' => function (Builder $query) {
+                    $query->where('type', ClipVoteType::Public);
+                },
+            ]
+        );
+    }
+
+    /**
+     * Counts jury votes as `jury_votes`.
+     */
+    #[Scope]
+    protected function withJuryVoteCount(Builder $query): Builder
+    {
+        return $query->withCount(
+            [
+                'votes as jury_votes' => function (Builder $query) {
+                    $query->where('type', ClipVoteType::Jury);
+                },
+            ]
+        );
+    }
+
+    /**
+     * Counts Votes
+     * - Jury votes as `jury_votes`
+     * - Public votes as `public_votes`
+     */
+    #[Scope]
+    protected function withVoteCount(Builder $query): Builder
+    {
+        return $query
+            ->withJuryVoteCount()
+            ->withPublicVoteCount();
     }
 }
