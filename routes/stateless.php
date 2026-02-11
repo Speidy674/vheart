6497e337622f506d504a47b3902c83d80265476b
Justin K. <justin.k@justplayer.de>
2026-02-11 08:04:44 +0100

    refactor image proxy to generic content proxy, technically allows anything but is limited to json and image mimetypes though

--- routes/stateless.php
+++ routes/stateless.php
@@ -2,15 +2,13 @@
 
 declare(strict_types=1);
 
-use App\Http\Controllers\ImageProxyController;
 use App\Http\Controllers\LocalesController;
+use App\Http\Controllers\ExternalContentProxyController;
 
 Route::get('/locales.json', LocalesController::class)
     ->middleware(['throttle:locales', 'cache.headers:public;max_age=3600;s_maxage=3600;stale_while_revalidate=86400;etag'])
     ->name('locales');
 
-Route::get('/static/proxy', ImageProxyController::class)
-    ->middleware([
-        'throttle:image-proxy',
-    ])
-    ->name('image-proxy');
+Route::get('/static-external/{type}/{identifier}.{extension}', ExternalContentProxyController::class)
+    ->middleware(['throttle:image-proxy'])
+    ->name('static-external');
