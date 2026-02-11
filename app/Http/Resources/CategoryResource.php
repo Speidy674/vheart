6cfaadb191834022917364b7972ff4ef1b43b0f6
Justin K. <justin.k@justplayer.de>
2026-02-12 00:07:31 +0100

    fix merge related stuff

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -3,12 +3,12 @@
 namespace App\Http\Resources;
 
 use App\Enums\ExternalContentProxyType;
-use App\Models\Game;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 /**
- * @mixin Game
+ * @mixin Category
  */
 class CategoryResource extends JsonResource
 {
