f1db9f06c70011d55f5db7b90bfd6c70eff78ee5
Justin K. <justin.k@justplayer.de>
2026-02-11 02:32:36 +0100

    remove css import, we already import it via vite

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,5 +1,3 @@
-import '../css/app.css';
-
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { StrictMode } from 'react';
