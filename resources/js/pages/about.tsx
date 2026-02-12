230884ad5c1341945f35a3d5ce86068ec27786e2
tawi <Itura.firestorm@gmail.com>
2026-02-12 13:06:16 +0100

    adding about-us changes

--- resources/js/pages/about.tsx
+++ resources/js/pages/about.tsx
@@ -6,7 +6,6 @@ import SpaceBackground from '@/components/spacebackground';
 import AppHeaderLayout from '@/layouts/app/app-header-layout';
 import { Head } from '@inertiajs/react';
 import { useTranslation } from 'react-i18next';
-import { BetterplaceDonationCard } from '@/components/aboutcard/betterplace-donation-card';
 
 export default function About({
     donationUrl,
@@ -20,24 +19,16 @@ export default function About({
     return (
         <AppHeaderLayout>
             <Head title={t('page_title')} />
-
                 <SpaceBackground />
-
                 <main className="pwepx-4 relative z-10 flex flex-1 items-center justify-center py-12">
                     <div className="w-full max-w-[1200px] space-y-8">
-                        <HeroCard t={t} />
+                        <HeroCard />
                         <DonationCard
-                            t={t}
                             donationUrl={donationUrl}
                             partnerIcon={partnerIcon}
                         />
-                        <BetterplaceDonationCard
-                            eventId={55712}
-                            maxVisibleDonations={8}
-                            refreshInterval={300000}
-                            projectId={0}
-                        />
-                        <ClipProcessCard t={t} />
+                        <BetterplaceDonationCard />
+                        <ClipProcessCard />
                     </div>
                 </main>
         </AppHeaderLayout>
