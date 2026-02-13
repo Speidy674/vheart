789cb1a48bd4f706de4c17635098e6b196c4af28
Justin K. <justin.k@justplayer.de>
2026-02-13 09:23:44 +0100

    new method to generate a valid twtich clip url

--- app/Http/Resources/PublicClipResource.php
+++ app/Http/Resources/PublicClipResource.php
@@ -26,7 +26,7 @@ class PublicClipResource extends JsonResource
             'slug' => $this->twitch_id,
             'title' => $this->title,
             'thumbnail_url' => URL::signedRoute('image-proxy', ['url' => $this->thumbnail_url]),
-            'clip_url' => $this->url,
+            'clip_url' => $this->getClipUrl(),
 
             'broadcaster' => $this->whenLoaded('broadcaster', [
                 'id' => $this->broadcaster_id,
