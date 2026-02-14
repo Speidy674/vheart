4a9305285deeb93eadb05471fc3de0ed276735b3
Justin K. <justin.k@justplayer.de>
2026-02-14 22:22:40 +0100

    remove timestamps from tags

--- database/migrations/2026_01_01_182641_create_tags_table.php
+++ database/migrations/2026_01_01_182641_create_tags_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
         Schema::create('tags', function (Blueprint $table): void {
             $table->id();
             $table->string('name')->unique();
-            $table->timestamps();
         });
 
         Schema::create('clip_tags', function (Blueprint $table) {
