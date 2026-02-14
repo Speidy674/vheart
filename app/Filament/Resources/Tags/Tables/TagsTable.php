89a3ac62e528df35dda0192d74c8a135892dacda
Justin K. <justin.k@justplayer.de>
2026-02-14 09:45:50 +0100

    tag filament resource

---
+++ app/Filament/Resources/Tags/Tables/TagsTable.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Filament\Resources\Tags\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+
+class TagsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->modifyQueryUsing(function (Builder $query) {
+                return $query->withCount('clips');
+            })
+            ->columns([
+                TextColumn::make('name'),
+                TextColumn::make('clips_count'),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
