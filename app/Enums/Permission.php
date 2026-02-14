779213388204aa00600fa17e995ea6ffec37e39a
Justin K. <justin.k@justplayer.de>
2026-02-14 09:44:30 +0100

    tag permissions

--- app/Enums/Permission.php
+++ app/Enums/Permission.php
@@ -61,6 +61,13 @@ enum Permission: string implements HasLabel
     case CreateComment = 'create_comment';
     case DeleteAnyComment = 'delete_any_comment';
 
+    // Tag
+    case ViewAnyTag = 'view_any_tag';
+    case ViewTag = 'view_tag';
+    case CreateTag = 'create_tag';
+    case UpdateAnyTag = 'update_any_tag';
+    case DeleteAnyTag = 'delete_any_tag';
+
     // Non-Model stuff
     case JuryVote = 'jury_vote';
 }
