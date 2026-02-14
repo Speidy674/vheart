57d2152bc100bb9a2986f62096f5eeb0bb74684c
Justin K. <justin.k@justplayer.de>
2026-02-14 09:44:40 +0100

    tag policy

--- app/Models/Clip/Tag.php
+++ app/Models/Clip/Tag.php
@@ -6,12 +6,15 @@ namespace App\Models\Clip;
 
 use App\Http\Resources\Clip\TagResource;
 use App\Models\Clip;
+use App\Policies\TagPolicy;
 use Database\Factories\Clip\TagFactory;
+use Illuminate\Database\Eloquent\Attributes\UsePolicy;
 use Illuminate\Database\Eloquent\Attributes\UseResource;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+#[UsePolicy(TagPolicy::class)]
 #[UseResource(TagResource::class)]
 class Tag extends Model
 {
