488e873b845abe5a5c23a2dd7d2d23a38c340c81
Justin K. <justin.k@justplayer.de>
2026-02-16 16:08:23 +0100

    import basic embed stuff

--- resources/js/static/app.ts
+++ resources/js/static/app.ts
@@ -6,8 +6,12 @@ import './bootstrap';
  */
 import Alpine from 'alpinejs';
 import exampleComponent from './components/example-component.js';
+import baseEmbed from './components/embeds/base-embed';
+import twitchEmbed from './components/embeds/twitch-embed';
 
 Alpine.data('exampleComponent', exampleComponent);
+Alpine.data('baseEmbed', baseEmbed);
+Alpine.data('twitchEmbed', twitchEmbed);
 
 window.Alpine = Alpine;
 Alpine.start();
