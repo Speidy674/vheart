f325aee7f8df03fe2cbef359e3a4566b32fc66a3
Justin K. <justin.k@justplayer.de>
2026-02-18 16:08:34 +0100

    (static) apply transform transition to clip preview

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
 
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
             @foreach($discover as $clip)
-                <x-clips.preview :clip="$clip" class="aspect-video hover:scale-105" />
+                <x-clips.preview :clip="$clip" class="aspect-video hover:scale-105 transition-transform" />
             @endforeach
         </div>
     </div>
