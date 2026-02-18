8f8a07c02322aa85a022cb9563581c34826ef1ef
Justin K. <justin.k@justplayer.de>
2026-02-18 15:54:07 +0100

    very basic static homepage version

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,59 +1,11 @@
-<x-layout title="VHeart Startseite">
+<x-layout title="Startseite">
+    <div class="m-auto w-full max-w-7xl space-y-4 p-4">
+        <x-embeds.youtube url="https://www.youtube-nocookie.com/embed/videoseries?list=PLPwib1xj01i4I_TqtyrRpnrjD2oaUknOn"/>
 
-    <div class="p-4 gap-4 grid grid-cols-2 w-full max-w-7xl m-auto">
-        <div>
-            <x-embeds.generic url="https://www.youtube-nocookie.com/embed/videoseries?list=UUUefW5IjMaQS_ZFaG4VZi9A"/>
-            <div class="flex items-center justify-between text-xs font-mono">
-                x-embeds.generic url="https://www.youtube-nocookie.com/embed/videoseries?list=UUUefW5IjMaQS_ZFaG4VZi9A"
-            </div>
-        </div>
-
-        <div>
-            <x-embeds.twitch
-                clip="HelpfulAmericanPeachDxCat-2gczKKTlc6MGYrPt"
-            />
-            <div class="flex items-center justify-between text-xs font-mono">
-                x-embeds.twitch clip="HelpfulAmericanPeachDxCat-2gczKKTlc6MGYrPt"
-            </div>
-        </div>
-
-        <div>
-            <x-embeds.youtube url="https://www.youtube-nocookie.com/embed/videoseries?list=UUUefW5IjMaQS_ZFaG4VZi9A" />
-            <div class="flex items-center justify-between text-xs font-mono">
-                x-embeds.youtube url="https://www.youtube-nocookie.com/embed/videoseries?list=UUUefW5IjMaQS_ZFaG4VZi9A"
-            </div>
-        </div>
-
-        <div>
-            <x-embeds.youtube yid="jvuk243L8z4" />
-            <div class="flex items-center justify-between text-xs font-mono">
-                x-embeds.youtube yid="jvuk243L8z4"
-            </div>
-        </div>
-
-        <div
-            x-data="{
-        clipList: @js($discover->pluck('twitch_id')->toArray(), JSON_THROW_ON_ERROR),
-
-        init() {
-            this.next();
-        },
-
-        next() {
-            let currentIndex = this.clipList.indexOf(this.activeClip);
-            let nextIndex = (currentIndex + 1) % this.clipList.length;
-
-            this.activeClip = this.clipList[nextIndex];
-        }
-    }"
-        >
-            <x-embeds.twitch
-                x-model="activeClip"
-            />
-            <div class="flex items-center justify-between">
-                <h2 x-text="activeClip"></h2>
-                <button @click="next()">next</button>
-            </div>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
+            @foreach($discover as $clip)
+                <x-clips.preview :clip="$clip" class="aspect-video hover:scale-105" />
+            @endforeach
         </div>
     </div>
 </x-layout>
