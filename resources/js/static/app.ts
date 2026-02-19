6b663511c275f2873ffbf6e66fd14c2e8964ae12
Justin K. <justin.k@justplayer.de>
2026-02-19 11:33:38 +0100

    (static) prepare stuff for lazy components

--- resources/js/static/app.ts
+++ resources/js/static/app.ts
@@ -5,21 +5,34 @@ import './bootstrap';
  *  @see https://alpinejs.dev/start-here
  */
 import Alpine from 'alpinejs';
+import AsyncAlpine from 'async-alpine';
 import intersect from '@alpinejs/intersect';
-import exampleComponent from './components/example-component.js';
+
+import image from '@/static/components/image';
 import baseEmbed from './components/embeds/base-embed';
 import twitchEmbed from './components/embeds/twitch-embed';
 import youtubeEmbed from '@/static/components/embeds/youtube-embed';
-import image from '@/static/components/image';
 
+Alpine.plugin(AsyncAlpine);
 Alpine.plugin(intersect);
 
 // Register alpine based components here, they can be used with `x-data="name({ ...config })"` in html
-Alpine.data('exampleComponent', exampleComponent);
+Alpine.data('image', image);
 Alpine.data('baseEmbed', baseEmbed);
 Alpine.data('twitchEmbed', twitchEmbed);
 Alpine.data('youtubeEmbed', youtubeEmbed);
-Alpine.data('image', image);
+
+// These Components (and their dependencies) will be bundled on their own and only
+// get loaded if they get used (or with very low prefetch priority)
+// Make sure they use the x-load attribute to tell alpine that they are lazy/async
+// @see https://async-alpine.dev/docs/
+const asyncComponents: [string, () => Promise<unknown>][] = [
+    ['example', () => import('@/static/components/example-component.js')],
+];
+
+asyncComponents.forEach(([componentName, importFn]) => {
+    Alpine.asyncData(componentName, importFn);
+});
 
 window.Alpine = Alpine;
 Alpine.start();
