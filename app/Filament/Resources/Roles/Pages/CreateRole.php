9f30de2abe7d4afe8963aecb553891c3747bdb70
Justin K. <justin.k@justplayer.de>
2026-02-19 07:40:14 +0100

    update filament role resource to support translations

--- app/Filament/Resources/Roles/Pages/CreateRole.php
+++ app/Filament/Resources/Roles/Pages/CreateRole.php
@@ -4,8 +4,19 @@ namespace App\Filament\Resources\Roles\Pages;
 
 use App\Filament\Resources\Roles\RoleResource;
 use Filament\Resources\Pages\CreateRecord;
+use LaraZeus\SpatieTranslatable\Actions\LocaleSwitcher;
+use LaraZeus\SpatieTranslatable\Resources\Pages\CreateRecord\Concerns\Translatable;
 
 class CreateRole extends CreateRecord
 {
+    use Translatable;
+
     protected static string $resource = RoleResource::class;
+
+    public function getHeaderActions(): array
+    {
+        return [
+            LocaleSwitcher::make(),
+        ];
+    }
 }
