1e10d7b66f1bb0926a6e1d597163639b82015de0
Justin K. <justin.k@justplayer.de>
2026-02-26 02:39:49 +0100

    (static) make background fixed

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -56,12 +56,12 @@
 </head>
 <body class="font-inter antialiased">
     <div class="pointer-events-none">
-        <div class="absolute inset-0 dark:block hidden"
+        <div class="fixed inset-0 dark:block hidden"
              style="background: radial-gradient(circle at 20% 30%, rgba(145, 70, 255, 0.20) 0%, rgba(10,10,26,0) 45%),
                                 radial-gradient(circle at 80% 70%, rgba(0, 174, 255, 0.14) 0%, rgba(10,10,26,0) 50%),
                                 #0a0a1a;">
         </div>
-        <div class="absolute inset-0 dark:hidden"
+        <div class="fixed inset-0 dark:hidden"
              style="background: radial-gradient(circle at 20% 30%, rgba(145, 70, 255, 0.15) 0%, rgba(255,255,255,0) 45%),
                                 radial-gradient(circle at 80% 70%, rgba(0, 174, 255, 0.15) 0%, rgba(255,255,255,0) 50%),
                                 #ffffff;">
