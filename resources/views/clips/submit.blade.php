83fd25c34f6a12ae8c02e0ba8dbd2e619fce6aae
Justin K. <justin.k@justplayer.de>
2026-03-01 17:05:30 +0100

    (static) alert component based on react version

---
+++ resources/views/clips/submit.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+    <x-ui.alert variant="destructive">
+        <x-ui.alert.title>Destructive Test</x-ui.alert.title>
+        <x-ui.alert.description>Dies ist ein test</x-ui.alert.description>
+    </x-ui.alert>
+    <x-ui.alert variant="success">
+        <x-ui.alert.title>Success Test</x-ui.alert.title>
+        <x-ui.alert.description>Dies ist ein test</x-ui.alert.description>
+    </x-ui.alert>
+</x-layout>
