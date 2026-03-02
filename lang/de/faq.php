90f0e0e562135ec0fc87c2afbe6fa395eef27f2c
Justin K. <justin.k@justplayer.de>
2026-03-03 00:26:34 +0100

    (static) update faq language files for german

--- lang/de/faq.php
+++ lang/de/faq.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 return [
     'title' => 'FAQ - Häufig gestelle Fragen',
-    'no-questions' => "We currently don't have any questions.",
+    'no-questions' => 'Wir haben aktuell keine fragen.',
     'search-no-javascript' => 'Suche ist nur verfügbar wenn JavaScript aktiviert ist.',
     'search-placeholder' => 'Fragen durchsuchen',
     'search-no-result' => 'Es konnten keine Fragen gefunden werden.',
