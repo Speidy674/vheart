277954954341e9e0f668032d3fab214820053d2b
Justin K. <justin.k@justplayer.de>
2026-03-04 06:38:40 +0100

    (feature flags) setup migration

---
+++ database/migrations/2026_03_04_041510_create_feature_flags_table.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('feature_flags', function (Blueprint $table) {
+            $table->string('name')->unique()->primary();
+            $table->boolean('enabled')->default(false);
+            $table->timestamps();
+        });
+    }
+};
