caf6220adbc963a7d1856f400660c8c960f5cf35
Justin K. <justin.k@justplayer.de>
2026-03-04 06:39:29 +0100

    (feature flags) setup config

---
+++ config/features.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    /*
+     * Add Feature flag overrides here
+     * this will override database values if set to something other than null
+     */
+
+    'placeholder' => env('FEATURE_PLACEHOLDER', null),
+];
