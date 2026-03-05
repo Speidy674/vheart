35a8b23b750f3e4e77c44c9f8d04805ed3e8e590
Justin K. <justin.k@justplayer.de>
2026-03-05 02:09:14 +0100

    (static) make client validation on two-factor input strict

--- resources/views/auth/two-factor.blade.php
+++ resources/views/auth/two-factor.blade.php
@@ -31,7 +31,8 @@
                             data-mode="{{ request('mode', 'otp') }}"
                             inputmode="{{ request('mode', 'otp') === 'otp' ? 'numeric' : 'text' }}"
                             pattern="{{ request('mode', 'otp') === 'otp' ? '[0-9]*' : '' }}"
-                            maxlength="{{ request('mode', 'otp') === 'otp' ? '6' : '' }}"
+                            minlength="{{ request('mode', 'otp') === 'otp' ? '6' : '21' }}"
+                            maxlength="{{ request('mode', 'otp') === 'otp' ? '6' : '21' }}"
                             placeholder="{{ request('mode', 'otp') === 'otp' ? '••••••' : __('auth.two-factor.form.backup.label') }}"
 
                             x-ref="input"
@@ -39,7 +40,8 @@
                             x-bind:disabled="processing"
                             x-bind:inputmode="mode === 'otp' ? 'numeric' : 'text'"
                             x-bind:pattern="mode === 'otp' ? '[0-9]*' : null"
-                            x-bind:maxlength="mode === 'otp' ? '6' : null"
+                            x-bind:minlength="mode === 'otp' ? '6' : '21'"
+                            x-bind:maxlength="mode === 'otp' ? '6' : '21'"
                             x-bind:placeholder="mode === 'otp' ? '••••••' : labels.backupLabel"
                         />
                         @error('code')
