6c2ea3646f9e73f63a78dd61c8f321b8b998a7a3
Justin K. <justin.k@justplayer.de>
2026-03-05 07:14:30 +0100

    (static) refactor and cleanup ClipVoteController for static version

--- app/Http/Controllers/ClipVoteController.php
+++ app/Http/Controllers/ClipVoteController.php
@@ -4,15 +4,13 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\Enums\Clips\CompilationStatus;
 use App\Enums\ClipVoteType;
 use App\Enums\Permission;
 use App\Models\Clip;
 use App\Models\Scopes\ClipPermissionScope;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Inertia\Inertia;
+use Symfony\Component\HttpFoundation\Response;
 
 class ClipVoteController extends Controller
 {
@@ -25,92 +23,117 @@ class ClipVoteController extends Controller
      */
     public function create(Request $request)
     {
-        $user = $request->user();
-        $session = $request->session();
+        $clipIdToVote = $this->getNextClipId($request);
 
-        return Inertia::render('evaluateclips', [
-            'clip' => Inertia::optional(function () use ($user, $session) {
+        $clip = Clip::query()
+            ->withoutGlobalScope(ClipPermissionScope::class)
+            ->withPublicVoteCount()
+            ->find($clipIdToVote);
 
-                $clipIdQueue = [];
-
-                if ($session->has(self::SESSION_QUEUE_KEY)) {
-                    $clipSessionQueue = $session->get(self::SESSION_QUEUE_KEY);
-                    if (is_array($clipSessionQueue) && $clipSessionQueue !== []) {
-                        $clipIdQueue = $clipSessionQueue;
-                    }
-                }
-
-                if ($clipIdQueue === []) {
-
-                    $clips = Clip::whereDoesntHave('votes', fn (Builder $query) => $query->where('user_id', $user->id))->whereNot('broadcaster_id', $user->id)
-                        ->whereNot('submitter_id', $user->id)
-                        ->whereDoesntHave('compilations', fn (Builder $query) => $query->whereIn('compilations.status', CompilationStatus::getVoteDisabledCases()))->select(['id'])
-                        ->inRandomOrder()
-                        ->limit(value: self::QUEUE_SIZE)
-                        ->get();
-
-                    $clipIdQueue = $clips->pluck('id')->toArray();
-
-                    $session->put(self::SESSION_QUEUE_KEY, $clipIdQueue);
-                }
-
-                if (count($clipIdQueue) === 0) {
-                    return null;
-                }
-
-                $clipIdToVote = $clipIdQueue[0];
-
-                $clip = Clip::withoutGlobalScope(ClipPermissionScope::class)->withCount(['votes' => function (Builder $query): void {
-                    $query->where('type', ClipVoteType::Public);
-                }])->find($clipIdToVote);
-
-                return $clip?->toResource();
-            }),
+        return view('clips.vote', [
+            'clip' => $clip,
         ]);
     }
 
     /**
      * Store the newly created resource in storage.
      */
-    public function store(Request $request): RedirectResponse
+    public function store(Request $request): Response
     {
-        $data = $request->validate([
-            'clip' => ['required', 'exists:clips,id'],
+        $request->validate([
             'voted' => ['required', 'bool'],
         ]);
 
+        $vote = $request->boolean('voted');
+        $clipId = $this->getNextClipId($request);
+        $this->shiftClipQueue($request);
+
+        $clip = Clip::query()
+            ->whereNotPublished()
+            ->find($clipId);
+
+        if ($clip) {
+            $voteType = $request->user()->can(Permission::JuryVote)
+                ? ClipVoteType::Jury
+                : ClipVoteType::Public;
+
+            $clip->votes()->updateOrCreate([
+                'user_id' => $request->user()->id,
+            ], [
+                'voted' => $vote,
+                'type' => $voteType,
+            ]);
+        }
+
+        if (! $request->expectsJson()) {
+            return back(fallback: route('vote'));
+        }
+
+        $clip = Clip::query()
+            ->withoutGlobalScope(ClipPermissionScope::class)
+            ->withPublicVoteCount()
+            ->find($this->getNextClipId($request));
+
+        return new JsonResponse($clip?->toResource());
+    }
+
+    /**
+     * Shifts the clip voting Queue
+     */
+    protected function shiftClipQueue(Request $request): void
+    {
+        $clipQueue = $this->getVoteQueue($request);
+
+        if (! $clipQueue || $clipQueue === []) {
+            return;
+        }
+
+        array_shift($clipQueue);
+        $request->session()->put(self::SESSION_QUEUE_KEY, $clipQueue);
+    }
+
+    /**
+     * Get the clip vote queue, if the queue is empty or does not exist we will also generate it here.
+     */
+    protected function getVoteQueue(Request $request): array
+    {
+        $user = $request->user();
         $session = $request->session();
+        $clips = $session->get(self::SESSION_QUEUE_KEY, []);
 
-        if ($session->has(key: self::SESSION_QUEUE_KEY)) {
-            $clipSessionQueue = $session->get(self::SESSION_QUEUE_KEY);
-            $clipIdtoVote = null;
-            if (is_array($clipSessionQueue) && $clipSessionQueue !== []) {
-                $clipIdtoVote = $clipSessionQueue[0];
+        if ($clips === []) {
+            $clips = Clip::query()
+                ->whereNot('submitter_id', $user->id)
+                ->whereNot('broadcaster_id', $user->id)
+                ->whereNot('creator_id', $user->id)
+                ->whereNoVotesFrom($user)
+                ->whereNotPublished()
+                ->inRandomOrder()
+                ->limit(self::QUEUE_SIZE)
+                ->pluck('id')
+                ->toArray();
+
+            if ($clips !== []) {
+                $session->put(self::SESSION_QUEUE_KEY, $clips);
             }
-
-            if (! empty($clipIdtoVote) && $clipIdtoVote === $data['clip']) {
-                array_shift($clipSessionQueue);
-                $session->put(self::SESSION_QUEUE_KEY, value: $clipSessionQueue);
-            }
         }
 
-        $clip = Clip::whereDoesntHave('compilations', fn (Builder $query) => $query->whereIn('compilations.status', CompilationStatus::getVoteDisabledCases()))->find($data['clip']);
+        return $clips;
+    }
 
-        if (empty($clip)) {
-            return back();
+    /**
+     * Get the next clip id in the queue.
+     *
+     * Will return `null` if there is nothing the user can vote on anymore.
+     */
+    protected function getNextClipId(Request $request): ?int
+    {
+        $voteQueue = $this->getVoteQueue($request);
+
+        if (count($voteQueue) !== 0) {
+            return $voteQueue[0];
         }
 
-        $voteType = ClipVoteType::Public;
-        if ($request->user()->can(Permission::JuryVote)) {
-            $voteType = ClipVoteType::Jury;
-        }
-        $clip->votes()->updateOrCreate([
-            'user_id' => $request->user()->id,
-        ], [
-            'voted' => $data['voted'],
-            'type' => $voteType,
-        ]);
-
-        return back();
+        return null;
     }
 }
