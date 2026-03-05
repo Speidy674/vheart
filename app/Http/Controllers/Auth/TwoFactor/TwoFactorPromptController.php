e2b494e2f5104a3e8abc68c06b3ba9b5e031e2bd
Justin K. <justin.k@justplayer.de>
2026-03-05 01:46:38 +0100

    (static) update 2fa controllers to use new blade version

--- app/Http/Controllers/Auth/TwoFactor/TwoFactorPromptController.php
+++ app/Http/Controllers/Auth/TwoFactor/TwoFactorPromptController.php
@@ -8,17 +8,16 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\TwoFactorChallengeRequest;
 use App\Models\User;
 use Filament\Auth\MultiFactor\App\AppAuthentication;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Routing\Controllers\HasMiddleware;
-use Inertia\Inertia;
-use Inertia\Response as InertiaResponse;
 
 /**
  * Shows the 2FA Verification Prompt
  */
 class TwoFactorPromptController extends Controller implements HasMiddleware
 {
-    public function __invoke(TwoFactorChallengeRequest $request, AppAuthentication $mfa): InertiaResponse|RedirectResponse
+    public function __invoke(TwoFactorChallengeRequest $request, AppAuthentication $mfa): View|RedirectResponse
     {
         $userId = $request->getChallengedUserId();
         $user = User::query()->find($userId);
@@ -27,7 +26,7 @@ class TwoFactorPromptController extends Controller implements HasMiddleware
             return to_route('login');
         }
 
-        return Inertia::render('auth/challenge');
+        return view('auth.two-factor');
     }
 
     public static function middleware(): array
