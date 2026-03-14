4018667f68e0db46ff0c7ae05e9d6d43d6291ccd
Speidy674 <marvin33@live.de>
2026-03-15 00:17:46 +0100

    remove old dashboard

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -5,55 +5,10 @@ declare(strict_types=1);
 use App\Enums\FeatureFlag;
 use App\Http\Controllers\Broadcaster\OnboardingController;
 use App\Http\Controllers\Broadcaster\OnboardingSubmitController;
-use App\Http\Middleware\BroadcasterDashboard;
 use App\Http\Middleware\FeatureFlagGuard;
-use App\Http\Middleware\RequiresBroadcasterProfile;
-use App\Models\Clip;
-use App\Models\Scopes\ClipPermissionScope;
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::middleware(['auth', FeatureFlagGuard::of(FeatureFlag::BroadcasterOnboarding)])->group(function () {
     Route::get('/onboarding', OnboardingController::class)->name('dashboard.onboarding');
     Route::post('/onboarding', OnboardingSubmitController::class)->name('dashboard.onboarding.store');
-});
-
-Route::middleware(['auth', FeatureFlagGuard::of(FeatureFlag::UserDashboard)])->group(function () {
-    Route::get('/dashboard2', function (Request $request) {
-        return Redirect::route('dashboard.main', $request->user()->id);
-    })
-        ->middleware([RequiresBroadcasterProfile::class])
-        ->name('dashboard');
-
-    Route::middleware(BroadcasterDashboard::class)
-        ->missing(function () {
-            return Redirect::route('home');
-        })->group(function () {
-            Route::get('/dashboard2/{user}', function (User $user, Request $request) {
-                return Inertia::render('dashboard/main');
-            })->name('dashboard.main');
-
-            Route::get('/dashboard2/{user}/clips', function (User $user, Request $request) {
-
-                return Inertia::render('dashboard/clips', [
-                    'clips' => Inertia::scroll(static function () use ($user) {
-                        $clip = Clip::query()
-                            ->withCount(['votes' => fn ($q) => $q->where('voted', true)->where('type', App\Enums\ClipVoteType::Public)])
-                            ->orderByDesc('id')
-                            ->where('broadcaster_id', $user->id)
-                            ->withoutGlobalScope(ClipPermissionScope::class)
-                            ->cursorPaginate();
-
-                        return $clip->toResourceCollection();
-                    }),
-                ]);
-            })->name('dashboard.clips');
-
-            Route::get('/dashboard2/{user}/permissions', function (User $user, Request $request) {
-                return Inertia::render('dashboard/permissions');
-            })->name('dashboard.permissions');
-        });
 });
