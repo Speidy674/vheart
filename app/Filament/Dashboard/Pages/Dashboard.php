087fcd42ab9dfed550388724f8f056f90c0543b0
Speidy674 <marvin33@live.de>
2026-03-17 23:22:02 +0100

    broadcaster dashboard - moved to luicde icons

--- app/Filament/Dashboard/Pages/Dashboard.php
+++ app/Filament/Dashboard/Pages/Dashboard.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace App\Filament\Dashboard\Pages;
 
+use App\Enums\Filament\LucideIcon;
+use BackedEnum;
 use Filament\Pages\Dashboard as BaseDashboard;
 
 class Dashboard extends BaseDashboard
 {
+    protected static string|BackedEnum|null $navigationIcon = LucideIcon::House;
+
     public static function getNavigationLabel(): string
     {
         return __('dashboard/navigation.dashboard');
