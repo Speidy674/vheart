4b97a728a3abf3db7c855f8e8f589f1992e847c1
Andreas Nedbal <andy@pixelde.su>
2026-03-17 02:29:21 +0100

    Reformat Blade views

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-  <x-about-us.hero-card></x-about-us.hero-card>
-  <x-about-us.betterplace-donation-card></x-about-us.betterplace-donation-card>
-  <x-about-us.clip-process-card></x-about-us.clip-process-card>
+    <x-about-us.hero-card></x-about-us.hero-card>
+    <x-about-us.betterplace-donation-card></x-about-us.betterplace-donation-card>
+    <x-about-us.clip-process-card></x-about-us.clip-process-card>
 </x-layout>
