c7571656a174dce5a25d2405baa103154e4eaa74
Justin K. <justin.k@justplayer.de>
2026-03-19 19:14:44 +0100

    add german translation for dashboard settings page and broadcaster

---
+++ lang/de/broadcaster.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'enums' => [
+        'broadcaster-consent' => [
+            'compilations' => 'Compilations',
+            'compilations_description' => 'Erlaubt es uns, deine inhalte in unseren Compilations zu verwenden.',
+            'shorts' => 'Shorts',
+            'shorts_description' => 'Erlaubt es uns, deine inhalte in unseren Shorts zu verwenden.',
+        ],
+        'dashboard-navigation-group' => [
+            'settings' => 'Einstellungen',
+        ],
+        'dashboard-navigation-item' => [
+            'manage-consent' => 'Zustimmung Verwalten',
+        ],
+    ],
+];
