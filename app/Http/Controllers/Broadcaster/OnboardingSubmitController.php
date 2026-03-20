92d5b5c734617bce87fb5fe65054513cf9ed93fe
Justin K. <justin.k@justplayer.de>
2026-03-20 09:13:36 +0100

    add consentlog entry in onboarding

--- app/Http/Controllers/Broadcaster/OnboardingSubmitController.php
+++ app/Http/Controllers/Broadcaster/OnboardingSubmitController.php
@@ -8,6 +8,7 @@ use App\Enums\FeatureFlag;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Broadcaster\OnboardingRequest;
 use App\Models\Broadcaster\Broadcaster;
+use App\Models\Broadcaster\BroadcasterConsentLog;
 use App\Support\FeatureFlag\Feature;
 use Filament\Facades\Filament;
 
@@ -15,12 +16,20 @@ class OnboardingSubmitController extends Controller
 {
     public function __invoke(OnboardingRequest $request)
     {
-        $broadcaster = Broadcaster::updateOrCreate(['id' => auth()->user()->id], [
+        $broadcaster = Broadcaster::updateOrCreate(['id' => auth()->id()], [
             'consent' => $request->array('consent'),
             'submit_user_allowed' => $request->boolean('everyone'),
             'submit_vip_allowed' => $request->boolean('vips'),
             'submit_mods_allowed' => $request->boolean('moderators'),
             'onboarded_at' => now(),
+        ]);
+
+        BroadcasterConsentLog::create([
+            'broadcaster_id' => $broadcaster->id,
+            'state' => $broadcaster->consent?->values(),
+            'changed_by' => auth()->id(),
+            'change_reason' => 'Self Onboarding',
+            'changed_at' => now(),
         ]);
 
         $fallbackRoute = Feature::isActive(FeatureFlag::UserDashboard) ? Filament::getPanel('dashboard')->getUrl($broadcaster) : route('home');
