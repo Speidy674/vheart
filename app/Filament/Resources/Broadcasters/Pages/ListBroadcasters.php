2cb0e373ad57a3109d8945f506c5c4277f1eb9c8
Justin K. <justin.k@justplayer.de>
2026-03-28 03:17:47 +0100

    redirect to broadcaster profile after manual creation

--- app/Filament/Resources/Broadcasters/Pages/ListBroadcasters.php
+++ app/Filament/Resources/Broadcasters/Pages/ListBroadcasters.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Filament\Resources\Broadcasters\Pages;
 
 use App\Filament\Resources\Broadcasters\BroadcasterResource;
+use App\Models\Broadcaster\Broadcaster;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
@@ -15,7 +16,10 @@ class ListBroadcasters extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make(),
+            CreateAction::make()
+                ->after(fn (Broadcaster $record, $livewire) => $livewire->redirect(
+                    BroadcasterResource::getUrl('view', ['record' => $record])
+                )),
         ];
     }
 }
