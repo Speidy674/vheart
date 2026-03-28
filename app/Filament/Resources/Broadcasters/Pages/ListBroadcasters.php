e1966c5741d64c4da6461845550182bf73c8a877
Justin K. <justin.k@justplayer.de>
2026-03-28 22:30:23 +0100

    restore soft-deleted broadcaster profiles if they still exist (and we have restore permission)

--- app/Filament/Resources/Broadcasters/Pages/ListBroadcasters.php
+++ app/Filament/Resources/Broadcasters/Pages/ListBroadcasters.php
@@ -7,6 +7,7 @@ namespace App\Filament\Resources\Broadcasters\Pages;
 use App\Filament\Resources\Broadcasters\BroadcasterResource;
 use App\Models\Broadcaster\Broadcaster;
 use Filament\Actions\CreateAction;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
 
 class ListBroadcasters extends ListRecords
@@ -17,6 +18,28 @@ class ListBroadcasters extends ListRecords
     {
         return [
             CreateAction::make()
+                ->using(function (array $data, string $model): Broadcaster {
+                    $existing = $model::withTrashed()
+                        ->where('id', $data['id'])
+                        ->first();
+
+                    if ($existing?->trashed()) {
+                        if (! auth()->user()->can('restore', $existing)) {
+                            Notification::make()
+                                ->title('You are not authorized to restore this broadcaster.')
+                                ->danger()
+                                ->send();
+
+                            $this->halt();
+                        }
+
+                        $existing->restore();
+
+                        return $existing;
+                    }
+
+                    return $model::create($data);
+                })
                 ->after(fn (Broadcaster $record, $livewire) => $livewire->redirect(
                     BroadcasterResource::getUrl('view', ['record' => $record])
                 )),
