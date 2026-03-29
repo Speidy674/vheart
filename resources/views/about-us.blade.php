4210194d0ef6f19c5ec6295236dd63dc0e06d35d
Andreas Nedbal <andy@pixelde.su>
2026-03-29 19:02:28 +0200

    Set page title via layout

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -1,4 +1,4 @@
-<x-layout>
+<x-layout :title="__('about.page_title')">
     <x-about-us.hero-card></x-about-us.hero-card>
     <x-about-us.betterplace-donation-card></x-about-us.betterplace-donation-card>
     <x-about-us.clip-process-card></x-about-us.clip-process-card>
