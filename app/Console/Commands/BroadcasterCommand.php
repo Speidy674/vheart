e0e49ff1a183c0dd786d6b6849bb7257a5617f9d
Justin K. <justin.k@justplayer.de>
2026-03-31 18:10:56 +0200

    command to allow broadcaster modification from terminal

---
+++ app/Console/Commands/BroadcasterCommand.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use App\Enums\Broadcaster\BroadcasterConsent;
+use App\Models\Broadcaster\Broadcaster;
+use App\Models\User;
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Command\Command as CommandAlias;
+
+use function Laravel\Prompts\multiselect;
+use function Laravel\Prompts\search;
+
+class BroadcasterCommand extends Command
+{
+    /**
+     * @var string
+     */
+    protected $signature = 'app:broadcaster';
+
+    /**
+     * @var string
+     */
+    protected $description = 'Create or Update a broadcaster profile for a user';
+
+    public function handle(): int
+    {
+        $id = search(
+            label: 'User',
+            options: fn (string $value) => $value !== ''
+                ? User::whereLike('name', "%{$value}%")->limit(50)->pluck('name', 'id')->all()
+                : []
+        );
+
+        $broadcaster = Broadcaster::createOrFirst(['id' => $id]);
+
+        if ($broadcaster->wasRecentlyCreated) {
+            $this->info('Created Broadcaster profile.');
+        }
+
+        // Consent
+        $prefix = 'consent_';
+
+        $currentConsent = $broadcaster->consent
+            ?->map(fn (BroadcasterConsent $c) => $prefix.$c->value)
+            ->all() ?? [];
+
+        $consent = multiselect(
+            label: 'Consent',
+            options: collect(BroadcasterConsent::cases())
+                ->mapWithKeys(fn (BroadcasterConsent $c) => [$prefix.$c->value => $c->name])
+                ->all(),
+            default: $currentConsent
+        );
+
+        $broadcaster->consent = collect($consent)
+            ->map(fn (string $value) => BroadcasterConsent::from((int) str_replace($prefix, '', $value)))
+            ->all();
+
+        // Submit Permissions
+        $booleanFields = [
+            'submit_user_allowed' => 'Everyone',
+            'submit_vip_allowed' => 'VIPs',
+            'submit_mods_allowed' => 'Mods',
+        ];
+        $currentSelections = collect($booleanFields)
+            ->filter(fn ($label, $field) => $broadcaster->{$field} === true)
+            ->keys()
+            ->all();
+
+        $selectedFields = multiselect(
+            label: 'Submit Permissions',
+            options: $booleanFields,
+            default: $currentSelections,
+            hint: 'Unselected will be disabled, Everyone will override other options.'
+        );
+
+        foreach ($booleanFields as $field => $label) {
+            $broadcaster->{$field} = in_array($field, $selectedFields, true);
+        }
+
+        $broadcaster->save();
+
+        return CommandAlias::SUCCESS;
+    }
+}
