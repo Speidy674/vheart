9c716ebefcf2931de8272a2efd06df02fa538ead
Justin K. <justin.k@justplayer.de>
2026-03-31 22:19:29 +0200

    implement compilation clip factory

--- database/factories/Clip/CompilationClipFactory.php
+++ database/factories/Clip/CompilationClipFactory.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace Database\Factories\Clip;
 
+use App\Enums\Clips\CompilationClipClaimStatus;
+use App\Models\Clip;
+use App\Models\Clip\Compilation;
 use App\Models\Clip\CompilationClip;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -15,7 +19,13 @@ class CompilationClipFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'clip_id' => Clip::factory(),
+            'compilation_id' => Compilation::factory(),
+            'added_by' => User::factory(),
+            'added_at' => fake()->dateTime(),
+            'claim_status' => CompilationClipClaimStatus::Pending,
+            'claimed_by' => null,
+            'claimed_at' => null,
         ];
     }
 }
