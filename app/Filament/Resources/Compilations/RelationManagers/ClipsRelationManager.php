aa4f16cca65ab428403330ca10d49feffc6d40da
Justin K. <justin.k@justplayer.de>
2026-03-31 22:21:19 +0200

    oops, fix wrong name

--- app/Filament/Resources/Compilations/RelationManagers/ClipsRelationManager.php
+++ app/Filament/Resources/Compilations/RelationManagers/ClipsRelationManager.php
@@ -178,11 +178,11 @@ class ClipsRelationManager extends RelationManager
                             $ids = array_diff($values, ['null']);
 
                             if (in_array('null', $values, true)) {
-                                $query->whereNull('compilation_clips.claimed_by');
+                                $query->whereNull('compilation_clip.claimed_by');
                             }
 
                             if ($ids !== []) {
-                                $query->orWhereIn('compilation_clips.claimed_by', $ids);
+                                $query->orWhereIn('compilation_clip.claimed_by', $ids);
                             }
                         });
                     }),
@@ -208,11 +208,11 @@ class ClipsRelationManager extends RelationManager
                             $ids = array_diff($values, ['null']);
 
                             if (in_array('null', $values, true)) {
-                                $query->whereNull('compilation_clips.added_by');
+                                $query->whereNull('compilation_clip.added_by');
                             }
 
                             if ($ids !== []) {
-                                $query->orWhereIn('compilation_clips.added_by', $ids);
+                                $query->orWhereIn('compilation_clip.added_by', $ids);
                             }
                         });
                     }),
@@ -238,7 +238,7 @@ class ClipsRelationManager extends RelationManager
                         blank: fn (Builder $query): Builder => $query,
                     ),
 
-                SelectFilter::make('compilation_clips.claim_status')
+                SelectFilter::make('compilation_clip.claim_status')
                     ->label('admin/resources/compilations.relation_managers.clips.filters.cutter_status')
                     ->translateLabel()
                     ->multiple()
