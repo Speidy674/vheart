235e82eb2b0eb892d13d33965eb6144d2f3e68cd
Justin K. <justin.k@justplayer.de>
2026-03-31 22:17:45 +0200

    properly setup compilation clip factory

--- app/Models/Clip/CompilationClip.php
+++ app/Models/Clip/CompilationClip.php
@@ -7,6 +7,7 @@ namespace App\Models\Clip;
 use App\Enums\Clips\CompilationClipClaimStatus;
 use App\Models\Traits\Auditable;
 use App\Models\User;
+use Database\Factories\Clip\CompilationClipFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\Pivot;
@@ -18,6 +19,8 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 class CompilationClip extends Pivot
 {
     use Auditable;
+
+    /** @use HasFactory<CompilationClipFactory> */
     use HasFactory;
 
     public $incrementing = true;
--- database/factories/Clip/CompilationClipFactory.php
+++ database/factories/Clip/CompilationClipFactory.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace Database\Factories\Clip;
 
+use App\Models\Clip\CompilationClip;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+/**
+ * @extends Factory<CompilationClip>
+ */
 class CompilationClipFactory extends Factory
 {
     public function definition(): array
