7ce3b9405e907ef47d365268fd37e6819effdf33
Justin K. <justin.k@justplayer.de>
2026-04-01 23:47:08 +0200

    update settings page to show the (current) download link
    most users wont have 2fa currently, so its maybe worth adding later but for now this is enough

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -2,23 +2,54 @@
 @use(Filament\Facades\Filament)
 <x-layout class="max-w-3xl w-full mx-auto space-y-6" :title="__('user.settings.title')">
     @feature(FeatureFlag::UserDashboard)
-        <x-ui.alert>
-            <x-ui.alert.title>
-                {{ __('user.settings.broadcaster_note.label') }}
-            </x-ui.alert.title>
-            <x-ui.alert.description class="flex flex-col md:flex-row items-center justify-between">
-                <p>
-                    {{ __('user.settings.broadcaster_note.description') }}
-                </p>
-                <x-ui.button href="{{ Filament::getPanel('dashboard')->getUrl() }}" variant="link">
-                    <span class="md:hidden">
-                        {{ __('user.settings.broadcaster_note.link') }}
-                    </span>
-                    <x-lucide-external-link defer class="size-5" />
-                </x-ui.button>
-            </x-ui.alert.description>
-        </x-ui.alert>
+    <x-ui.alert>
+        <x-ui.alert.title>
+            {{ __('user.settings.broadcaster_note.label') }}
+        </x-ui.alert.title>
+        <x-ui.alert.description class="flex flex-col md:flex-row items-center justify-between">
+            <p>
+                {{ __('user.settings.broadcaster_note.description') }}
+            </p>
+            <x-ui.button href="{{ Filament::getPanel('dashboard')->getUrl() }}" variant="link">
+                        <span class="md:hidden">
+                            {{ __('user.settings.broadcaster_note.link') }}
+                        </span>
+                <x-lucide-external-link defer class="size-5"/>
+            </x-ui.button>
+        </x-ui.alert.description>
+    </x-ui.alert>
     @endfeature
+
+    <x-ui.card class="shadow-sm">
+        <x-ui.card.header class="pb-6 border-b border-border">
+            <x-ui.card.title class="text-center text-2xl font-bold tracking-tight">
+                <h1>{{ __('user.settings.data-export.heading') }}</h1>
+            </x-ui.card.title>
+        </x-ui.card.header>
+
+        <x-ui.card.content class="p-4 pt-6 space-y-8">
+            <section id="data-export" class="space-y-6">
+                <div class="space-y-1">
+                    <h3 class="text-base font-semibold text-foreground">
+                        {{ __('user.settings.data-export.subheading') }}
+                    </h3>
+                    <p class="text-sm text-muted-foreground">
+                        {{ __('user.settings.data-export.description') }}
+                    </p>
+                </div>
+
+                <form method="get" action="{{ route('user.settings.data-export') }}" class="space-y-6">
+                    @method('get')
+
+                    <div class="flex items-center gap-4">
+                        <x-ui.button type="submit" variant="outline">
+                            {{ __('user.settings.data-export.submit') }}
+                        </x-ui.button>
+                    </div>
+                </form>
+            </section>
+        </x-ui.card.content>
+    </x-ui.card>
 
     <x-ui.card class="shadow-sm">
         <x-ui.card.header class="pb-6 border-b border-border">
@@ -47,8 +78,8 @@
                                 {{ __('user.settings.delete.confirmation.two-factor.label') }}
                                 Gib zur Bestätigung deinen Zwei-Faktor Code ein
                             </label>
-                            <x-ui.input.two-factor />
-                            <x-ui.button.two-factor-toggle />
+                            <x-ui.input.two-factor/>
+                            <x-ui.button.two-factor-toggle/>
                         </div>
 
                         @pushonce('elements')
@@ -84,12 +115,13 @@
                         @endpushonce
                     @else
                         <div class="space-y-2">
-                            <label for="code" class="block text-sm font-medium [&_code]:font-mono [&_code]:border [&_code]:px-1 [&_code]:py-0.5 [&_code]:rounded [&_code]:bg-muted [&_code]:select-none">
+                            <label for="code"
+                                   class="block text-sm font-medium [&_code]:font-mono [&_code]:border [&_code]:px-1 [&_code]:py-0.5 [&_code]:rounded [&_code]:bg-muted [&_code]:select-none">
                                 {!! __('user.settings.delete.confirmation.keyword.label', ['keyword' => __('user.settings.delete.confirmation.keyword.keyword')]) !!}
                             </label>
-                            <x-ui.input id="code" name="code" type="text" required />
+                            <x-ui.input id="code" name="code" type="text" required/>
                             @error('code')
-                                <p class="text-sm text-destructive">{{ $message }}</p>
+                            <p class="text-sm text-destructive">{{ $message }}</p>
                             @enderror
                         </div>
                     @endif
