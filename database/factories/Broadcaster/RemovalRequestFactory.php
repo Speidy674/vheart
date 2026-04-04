7418217d7584dbf69c4b6b42d4ad5b19fcc53e87
Justin K. <justin.k@justplayer.de>
2026-04-04 16:19:28 +0200

    clip removal request factory

---
+++ database/factories/Broadcaster/RemovalRequestFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories\Broadcaster;
+
+use App\Enums\Broadcaster\RemovalRequestStatus;
+use App\Models\Broadcaster\Broadcaster;
+use App\Models\Broadcaster\RemovalRequest;
+use App\Models\Clip;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<RemovalRequest>
+ */
+class RemovalRequestFactory extends Factory
+{
+    /**
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'broadcaster_id' => Broadcaster::factory(),
+            'clip_id' => Clip::factory(),
+            'status' => RemovalRequestStatus::Pending,
+            'claimed_by' => null,
+            'claimed_at' => null,
+            'resolved_by' => null,
+            'resolved_at' => null,
+        ];
+    }
+}
