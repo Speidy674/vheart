19d7013e09af2387771938b7250d5d6ba4686c83
Justin K. <justin.k@justplayer.de>
2026-04-04 17:07:25 +0200

    add whereEligibleForArchival scope to clips model

--- app/Models/Clip.php
+++ app/Models/Clip.php
@@ -212,6 +212,30 @@ class Clip extends Model implements Commentable, ExternalProxyable
     }
 
     /**
+     * Get Clips that should be archived
+     *
+     * We will allow a 1 week buffer before we permanently archive clips though, just in case something changes
+     */
+    #[Scope]
+    protected function whereEligibleForArchival(Builder $query): Builder
+    {
+        /** @var CarbonInterval $maxAge */
+        $maxAge = config('vheart.clips.voting.maximum_age');
+
+        return $query
+            ->whereNull(['final_jury_votes', 'final_public_votes', 'final_score'])
+            ->where(function (Builder $query) use ($maxAge): void {
+                $query
+                    ->where('created_at', '<', now()->sub($maxAge)->subWeek())
+                    ->orWhereHas('compilations',
+                        fn (Builder $q) => $q
+                            ->whereIn('compilations.status', CompilationStatus::getVoteDisabledCases())
+                            ->where('compilations.updated_at', '<', now()->subWeek())
+                    );
+            });
+    }
+
+    /**
      * Exclude Clips that has been Submitted before a date
      */
     #[Scope]
