4b95b92964d73f6368d31835c2393e72837c662f
Speidy674 <marvin33@live.de>
2026-04-07 23:46:08 +0200

    removed query

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -5,19 +5,13 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Models\Role;
-use App\Models\User;
 use Illuminate\Contracts\View\View;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class TeamController extends Controller
 {
     public function __invoke(Request $request): View
     {
-        $total = User::query()
-            ->whereHas('roles', fn (Builder $builder) => $builder->where('public', true))
-            ->count();
-
         $roles = Role::query()
             ->where('public', true)
             ->orderBy('weight', 'desc')
