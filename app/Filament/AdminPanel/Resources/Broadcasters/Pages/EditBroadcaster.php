58f8ce13b3cb5bb5893f5666c7ad341f9a1714bb
Justin K. <justin.k@justplayer.de>
2026-04-10 09:02:09 +0200

    refactor edit broadcaster page to also append to broadcaster consent log
    that was actually a bug i fixed in the last commit that it was automatically created oops

--- app/Filament/AdminPanel/Resources/Broadcasters/Pages/EditBroadcaster.php
+++ app/Filament/AdminPanel/Resources/Broadcasters/Pages/EditBroadcaster.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 namespace App\Filament\AdminPanel\Resources\Broadcasters\Pages;
 
+use App\Enums\Broadcaster\BroadcasterConsent;
 use App\Filament\AdminPanel\Resources\Broadcasters\BroadcasterResource;
+use App\Models\Broadcaster\Broadcaster;
+use App\Models\Broadcaster\BroadcasterConsentLog;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\ForceDeleteAction;
 use Filament\Actions\RestoreAction;
 use Filament\Actions\ViewAction;
 use Filament\Resources\Pages\EditRecord;
+use JsonException;
 
 class EditBroadcaster extends EditRecord
 {
@@ -23,5 +27,34 @@ class EditBroadcaster extends EditRecord
             ForceDeleteAction::make(),
             RestoreAction::make(),
         ];
+    }
+
+    /**
+     * @throws JsonException
+     */
+    protected function afterSave(): void
+    {
+        /** @var Broadcaster $broadcaster */
+        $broadcaster = $this->record;
+
+        $newState = $broadcaster->consent->values()
+            ->map(fn (BroadcasterConsent $e) => $e->value)
+            ->all();
+
+        $alreadyLogged = $broadcaster->consentLogs()
+            ->where('state', json_encode($newState, JSON_THROW_ON_ERROR))
+            ->latest('changed_at')
+            ->exists();
+
+        if ($alreadyLogged) {
+            return;
+        }
+
+        BroadcasterConsentLog::create([
+            'broadcaster_id' => $broadcaster->id,
+            'state' => $newState,
+            'changed_by' => auth()->id(),
+            'changed_at' => now(),
+        ]);
     }
 }
