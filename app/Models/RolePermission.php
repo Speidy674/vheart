d65fe1bd648c5758612cf9e5df80889bca905536
Justin K. <justin.k@justplayer.de>
2026-04-11 04:51:27 +0200

    use WithoutIncrementing attribute on RolePermission model

--- app/Models/RolePermission.php
+++ app/Models/RolePermission.php
@@ -6,15 +6,15 @@ namespace App\Models;
 
 use App\Enums\Permission;
 use Illuminate\Database\Eloquent\Attributes\Table;
+use Illuminate\Database\Eloquent\Attributes\WithoutTimestamps;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
 #[Table(name: 'role_permissions')]
+#[WithoutTimestamps]
 class RolePermission extends Pivot
 {
     use HasFactory;
-
-    public $timestamps = false;
 
     protected $casts = [
         'permission' => Permission::class,
