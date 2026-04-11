982b31e9e5d843630e32932ea6f445739e74186f
Justin K. <justin.k@justplayer.de>
2026-04-11 05:41:04 +0200

    move factory model definition to attribute in BroadcasterConsentLogFactory

--- database/factories/BroadcasterConsentLogFactory.php
+++ database/factories/BroadcasterConsentLogFactory.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Database\Factories;
 
+use Illuminate\Database\Eloquent\Factories\Attributes\UseModel;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+#[UseModel(BroadcasterConsentLogFactory::class)]
 class BroadcasterConsentLogFactory extends Factory
 {
     public function definition(): array
