7aa7e667159fb931fa8b88c7bc0f9f51a2b53542
Justin K. <justin.k@justplayer.de>
2026-04-11 05:41:15 +0200

    move factory model definition to attribute in AuditFactory

--- database/factories/AuditFactory.php
+++ database/factories/AuditFactory.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace Database\Factories;
 
 use App\Models\Audit;
+use Illuminate\Database\Eloquent\Factories\Attributes\UseModel;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
  * @extends Factory<Audit>
  */
+#[UseModel(Audit::class)]
 class AuditFactory extends Factory
 {
     /**
