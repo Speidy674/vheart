fd3f0916a39dd7beda299d3224b35968b16f37b6
Justin K. <justin.k@justplayer.de>
2026-04-12 11:13:18 +0200

    update BroadcasterConsentLogFactory to properly fill in state for faked entries

--- database/factories/Broadcaster/BroadcasterConsentLogFactory.php
+++ database/factories/Broadcaster/BroadcasterConsentLogFactory.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Database\Factories\Broadcaster;
 
+use App\Models\Broadcaster\Broadcaster;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class BroadcasterConsentLogFactory extends Factory
@@ -11,7 +13,10 @@ class BroadcasterConsentLogFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'broadcaster_id' => Broadcaster::factory(),
+            'state' => [],
+            'changed_by' => User::factory(),
+            'changed_at' => now(),
         ];
     }
 }
