e8a9720ed502de5a37d48e8daa497d38912094f1
Justin K. <justin.k@justplayer.de>
2026-04-12 11:27:34 +0200

    merge change from master to BroadcasterConsentLogFactory

--- database/factories/Broadcaster/BroadcasterConsentLogFactory.php
+++ database/factories/Broadcaster/BroadcasterConsentLogFactory.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 namespace Database\Factories\Broadcaster;
 
 use App\Models\Broadcaster\Broadcaster;
+use App\Models\Broadcaster\BroadcasterConsentLog;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Attributes\UseModel;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+#[UseModel(BroadcasterConsentLog::class)]
 class BroadcasterConsentLogFactory extends Factory
 {
     public function definition(): array
