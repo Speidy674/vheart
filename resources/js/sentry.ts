7e973156c8f7f7c9c245efd168d81997539daf88
Justin K. <justin.k@justplayer.de>
2026-04-17 19:04:34 +0200

    update sentry to add some placeholders and debug logging for us

--- resources/js/sentry.ts
+++ resources/js/sentry.ts
@@ -14,5 +14,20 @@ if (dsn) {
         replaysOnErrorSampleRate: 0,
         profileSessionSampleRate: 0,
         enableLogs: false,
+        integrations: [
+            // glitch does not seem to support this yet
+            // Sentry.feedbackIntegration({
+            //     colorScheme: 'system',
+            // }),
+        ],
+        beforeSend(event, hint) {
+            console.debug('sending error to tracker...', event, hint);
+
+            if (event.exception && event.event_id) {
+                // does not work currently, no need to annoy the user with that yet
+                //Sentry.showReportDialog({ eventId: event.event_id });
+            }
+            return event;
+        },
     });
 }
