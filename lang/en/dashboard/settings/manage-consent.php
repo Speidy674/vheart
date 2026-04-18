4267659ee1a2535384e067a932eebea027a3f533
Speidy674 <marvin33@live.de>
2026-04-18 13:21:48 +0200

    update lang files

--- lang/en/dashboard/settings/manage-consent.php
+++ lang/en/dashboard/settings/manage-consent.php
@@ -3,7 +3,13 @@
 declare(strict_types=1);
 
 return [
-    'section' => [
-        'description' => 'Changes may not take effect in the next episode if you revoke between Thu 00:00 and Fri 16:00.',
+    'sections' => [
+        'consent' => [
+            'description' => 'Changes may not take effect in the next episode if you revoke between Thu 00:00 and Fri 16:00.',
+        ],
+        'default_clip_status' => [
+            'label' => 'Default Clip Status',
+            'description' => 'Unrelated to who can submit clips, how would you like us to handle your Clips?',
+        ],
     ],
 ];
