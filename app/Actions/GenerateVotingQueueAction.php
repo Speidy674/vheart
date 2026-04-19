3b51292c2cf8ba2235a34bb0a94c05639b30f252
Justin K. <justin.k@justplayer.de>
2026-04-19 10:31:46 +0200

    just get rid of that helper completely

--- app/Actions/GenerateVotingQueueAction.php
+++ app/Actions/GenerateVotingQueueAction.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Actions;
 
 use App\Models\Broadcaster\Broadcaster;
-use Closure;
 use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -16,10 +15,16 @@ class GenerateVotingQueueAction
 
     public function execute(Authenticatable $user): array
     {
-        $broadcasters = Broadcaster::whereHas('clips', $this->eligibleClips($user))
+        $broadcasters = Broadcaster::query()
+            ->whereHas('clips', fn (Builder $q) => $q->whereEligibleForVoting($user))
             ->inRandomOrder()
             ->limit(self::QUEUE_SIZE)
-            ->with(['clips' => fn (HasMany $q): HasMany => $this->eligibleClips($user)($q)->select('id', 'broadcaster_id')->inRandomOrder()->limit(1)])
+            ->with([
+                'clips' => fn (HasMany $q) => $q->whereEligibleForVoting($user)
+                    ->select('id', 'broadcaster_id')
+                    ->inRandomOrder()
+                    ->limit(1),
+            ])
             ->get(['id']);
 
         return $broadcasters
@@ -29,10 +34,5 @@ class GenerateVotingQueueAction
             ->pluck('id')
             ->shuffle()
             ->toArray();
-    }
-
-    private function eligibleClips(Authenticatable $user): Closure
-    {
-        return static fn (HasMany|Builder $query): HasMany|Builder => $query->whereEligibleForVoting($user);
     }
 }
