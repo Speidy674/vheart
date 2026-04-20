99cd6c2a8d7f36fbc4095465fe269cc8f080a820
Justin K. <justin.k@justplayer.de>
2026-04-20 12:03:15 +0200

    support any table column component in HasFilamentTableColumn

--- app/Models/Contracts/HasFilamentTableColumn.php
+++ app/Models/Contracts/HasFilamentTableColumn.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 namespace App\Models\Contracts;
 
 use App\Filament\Tables\MorphColumn;
-use Filament\Tables\Columns\Column;
+use Filament\Tables\Columns\Layout\Component;
 
 /**
  * Provides a way for {@see MorphColumn} to render the model on a morphable relationship column.
  */
 interface HasFilamentTableColumn
 {
-    public static function getFilamentTableColumn(string $name): Column;
+    public static function getFilamentTableColumn(string $name): Component;
 }
